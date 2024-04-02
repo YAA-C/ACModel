35f0bb2d57c4fe5d70040ce537dbe20022a4c29f
SiddheshDhinge <saddy97531@gmail.com>
2024-04-03 01:05:16 +0530

    not working

--- src/FightGenerator.py
+++ src/FightGenerator.py
@@ -6,7 +6,7 @@ class FightGenerator:
         pass
 
 
-    def fights(df: pd.DataFrame):
+    def fights(self, df: pd.DataFrame):
         indexes: list = [-1]
         indexes.extend(df[df.isnull().all(axis=1)].index.to_list())
         
@@ -16,3 +16,13 @@ class FightGenerator:
             curDf: pd.DataFrame = df.iloc[prevRowIndex: curRowIndex, :].copy()
             curDf.reset_index(inplace = True)
             yield curDf
+
+
+    def getFightData(self, df: pd.DataFrame) -> list[list]:
+        x = df["X"]
+        y = df["Y"]
+        targetX = df["targetX"]
+        targetY = df["targetY"]
+        isHurt = ~df["targetId"].isna()
+        data = [x for x in zip(x, y, targetX, targetY, isHurt)]
+        return data
